269ee858163de676f4727814c0fa05699e37705e
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2024-02-09 18:49:20 +0200

    Added Push Button ECU

--- ECU_Layer/ECU_Push_Button/ecu_push_button.c
+++ ECU_Layer/ECU_Push_Button/ecu_push_button.c
@@ -9,3 +9,58 @@
  */
 
 #include "ecu_push_button.h"
+
+
+
+Std_ReturnType ecu_push_button_initialize(const button_t *btn) {
+    Std_ReturnType ret = E_OK;
+
+    if (btn == NULL) {
+        ret = E_NOT_OK;
+    } 
+    else {
+        ret = hal_gpio_pin_init(&(btn->button_pin));
+    }
+
+    return ret;
+}
+
+
+Std_ReturnType ecu_push_button_read_state(const button_t *btn, button_state_t *btn_state) {
+    Std_ReturnType ret = E_OK;
+    logic_t pin_logic_status = OUTPUT_LOGIC_LOW;
+
+    // NULL pointer check
+    if (btn == NULL || btn_state == NULL) 
+        return E_NOT_OK;
+    else {/* NULL */}
+
+    // Read logic on the pin, return either HIGH or LOW
+    hal_gpio_pin_read(&(btn->button_pin), &(pin_logic_status));        
+    
+    // In case it was active high -> PRESSED at logic HIGH
+    if (btn->button_connection == BUTTON_ACTIVE_HIGH)
+    {
+        if (pin_logic_status == OUTPUT_LOGIC_HIGH)
+            *btn_state = BUTTON_PRESSED;
+        else if (pin_logic_status == OUTPUT_LOGIC_LOW)
+            *btn_state = BUTTON_RELEASED;
+        else
+            ret = E_NOT_OK;
+    }
+    // In case it was active low -> PRESSED at logic LOW
+    else if (btn->button_connection == BUTTON_ACTIVE_LOW)
+    {
+        if (pin_logic_status == OUTPUT_LOGIC_HIGH)
+            *btn_state = BUTTON_RELEASED;
+        else if (pin_logic_status == OUTPUT_LOGIC_LOW)
+            *btn_state = BUTTON_PRESSED;
+        else
+            ret = E_NOT_OK;
+    }
+    else {
+        ret = E_NOT_OK;
+    }
+
+    return ret;
+}
--- ECU_Layer/ECU_Push_Button/ecu_push_button.h
+++ ECU_Layer/ECU_Push_Button/ecu_push_button.h
@@ -17,9 +17,29 @@
 
 /********* Section : Data Type Definition ************/
 
+typedef enum{
+    BUTTON_PRESSED = 0,
+    BUTTON_RELEASED
+}button_state_t;
+
+typedef enum{
+    BUTTON_ACTIVE_HIGH = 0,
+    BUTTON_ACTIVE_LOW
+}button_active_t;
+
+typedef struct{
+    pin_config_t button_pin;
+    button_state_t button_state : 1;
+    button_active_t button_connection : 1;
+    uint8 reserved : 6;
+}button_t;
+
 /********* Section : Macro Declaration ************/
 
 /********* Section : Function like Macro Definitions ************/
+
+Std_ReturnType ecu_push_button_initialize(const button_t *btn);
+Std_ReturnType ecu_push_button_read_state(const button_t *btn, button_state_t *btn_state);
 
 /********* Section : Function Definitions ************/
 
