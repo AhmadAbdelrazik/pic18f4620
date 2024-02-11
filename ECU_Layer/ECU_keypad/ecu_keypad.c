3166a73c414f0d6585f5a0c89c7e73025e8de803
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2024-02-11 15:55:15 +0200

    added ECU keypad

---
+++ ECU_Layer/ECU_keypad/ecu_keypad.c
@@ -0,0 +1,81 @@
+/**
+ * @file ecu_keypad.c
+ * 
+ * @author Ahmad Abdelrazik (ahmadabdelrazik159@gmail.com)
+ * 
+ * @linkedin linkedin.com/in/ahmadabdelrazik/
+ * @github github.com/AhmadAbdelrazik/
+ * 
+ * @date 2024-02-11
+ * 
+ * @brief 
+ */
+
+#include "ecu_keypad.h"
+
+
+uint8 btn_values[ECU_KEYPAD_ROW][ECU_KEYPAD_COLUMN] = {
+                                                        {'7','8','9','#'},
+                                                        {'4','5','6','*'},
+                                                        {'1','2','3','-'},
+                                                        {'#','0','=','+'}
+                                                      };
+
+
+
+Std_ReturnType ecu_keypad_initialize(const keypad_t *keypad)
+{
+    Std_ReturnType ret = E_OK;
+    uint8 l_row_counter, l_column_counter;
+
+    if (keypad == NULL)
+        return E_NOT_OK;
+
+    // Initialize each row pin    
+    for (l_row_counter = 0; l_row_counter < ECU_KEYPAD_ROW; l_row_counter++)
+        ret = hal_gpio_pin_init(&(keypad->row[l_row_counter]));
+
+    // Initialize each column pin
+    for (l_column_counter = 0; l_column_counter < ECU_KEYPAD_COLUMN; l_column_counter++)
+        ret = hal_gpio_pin_init(&(keypad->column[l_column_counter]));    
+
+    return ret;
+}
+
+Std_ReturnType ecu_keypad_get_value(const keypad_t *keypad, uint8 *value)
+{
+    Std_ReturnType ret = E_OK;
+    uint8 l_row_counter, l_column_counter, l_column_logic;
+    
+    if (keypad == NULL || value == NULL)
+        return E_NOT_OK;
+
+    
+    // Write 0 on all row pins
+    for (l_row_counter = 0; l_row_counter < ECU_KEYPAD_ROW; l_row_counter++)
+        ret = hal_gpio_pin_write(&(keypad->row[l_row_counter]), OUTPUT_LOGIC_LOW);
+
+    for (l_row_counter = 0; l_row_counter < ECU_KEYPAD_ROW; l_row_counter++)
+    {
+        // Write 1 on the current row
+        ret = hal_gpio_pin_write(&(keypad->row[l_row_counter]), OUTPUT_LOGIC_HIGH);
+        // write 0 on the previous row, (starting from the second row)
+        if (l_row_counter > 0)
+            ret = hal_gpio_pin_write(&(keypad->row[l_row_counter - 1]), OUTPUT_LOGIC_LOW);
+    
+        // check each column for Logic High value
+        for (l_column_counter = 0; l_column_counter < ECU_KEYPAD_COLUMN; l_column_counter++)
+        {
+            ret = hal_gpio_pin_read(&(keypad->column[l_column_counter]), &l_column_logic);
+            if (l_column_logic == OUTPUT_LOGIC_HIGH)
+            {
+                *value = btn_values[l_row_counter][l_column_counter];
+                return ret;
+            }
+        }
+
+    }
+
+
+    return ret;
+}
---
+++ ECU_Layer/ECU_keypad/ecu_keypad.h
@@ -0,0 +1,55 @@
+/**
+ * @file ecu_keypad.h
+ * 
+ * @author Ahmad Abdelrazik (ahmadabdelrazik159@gmail.com)
+ * 
+ * @linkedin linkedin.com/in/ahmadabdelrazik/
+ * @github github.com/AhmadAbdelrazik/
+ * 
+ * @date 2024-02-11
+ * 
+ * @brief 
+ */
+
+#ifndef ECU_KEYPAD_H
+#define ECU_KEYPAD_H
+
+/********* Section : Includes ************/
+
+#include "../../MCAL_Layer/HAL_gpio/hal_gpio.h"
+
+/********* Section : Macro Declaration ************/
+
+#define ECU_KEYPAD_ROW          4
+#define ECU_KEYPAD_COLUMN       4
+
+/********* Section : Data Type Definition ************/
+
+typedef struct {
+    pin_config_t row[ECU_KEYPAD_ROW];
+    pin_config_t column[ECU_KEYPAD_COLUMN];
+}keypad_t;
+
+
+/********* Section : Function like Macro Definitions ************/
+
+/********* Section : Function Definitions ************/
+
+/**
+ * @brief initialize the keypad and it's pins.
+ * 
+ * @param keypad 
+ * @return Std_ReturnType : E_OK if no errors, else E_NOT_OK
+ */
+Std_ReturnType ecu_keypad_initialize(const keypad_t *keypad);
+
+/**
+ * @brief get the value pressed.
+ * 
+ * @param keypad 
+ * @param value : store the value
+ * @return Std_ReturnType : E_OK if no errors, else E_NOT_OK
+ */
+Std_ReturnType ecu_keypad_get_value(const keypad_t *keypad, uint8 *value);
+
+#endif
